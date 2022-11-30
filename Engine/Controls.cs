ab44e01d6d0c210364d5e8dcb255bc3d695b4a9a
J-SilvaJunior <mokozuma@gmail.com>
2022-11-29 23:15:48 -0300

    Novo arquivo que determina os global using dos controles, para facilitar. Criando classe do botão simples.

---
+++ Engine/Controls.cs
@@ -0,0 +1,9 @@
+//
+global using System;
+global using System.Collections.Generic;
+global using Microsoft.Xna.Framework;
+global using Microsoft.Xna.Framework.Content;
+global using Microsoft.Xna.Framework.Graphics;
+global using Microsoft.Xna.Framework.Audio;
+global using Microsoft.Xna.Framework.Input;
+namespace MonoControls;
